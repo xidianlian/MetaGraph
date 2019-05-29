33131ada78fcfd3970f7be059fb45a51b689d6aa
“xidianlian <727610307@qq.com>
2019-05-29 08:35:14 +0800

    fusion

--- use7000mal.py
+++ use7000mal.py
@@ -31,60 +31,42 @@ app_api_data = pd.read_csv(api_csv_path, dtype=np.uint16)
 app_perm_data = pd.read_csv(perm_csv_path, dtype=np.uint16)
 app_method_data = pd.read_csv(method_csv_path, dtype=np.uint16)
 
-#count_series = mal_api_data.sum(axis = 1)
-#drop_feature = [index for index in count_series.index if count_series[index] < 100]
-#print(drop_feature)
-#app_api_data = app_api_data.drop(drop_feature, axis=1)
+def select_features(data,label,num):
+    selector = SelectKBest(score_func=chi2, k=num)
+    new_api_data = selector.fit_transform(data, label)
+    return pd.DataFrame(new_api_data)
 
-mal_perm_data = app_perm_data[app_perm_data['class'].isin([1])]
-mal_perm_data = mal_perm_data.drop(['class'], axis = 1)
-ben_perm_data = app_perm_data[app_perm_data['class'].isin([0])]
-ben_perm_data = ben_perm_data.drop(['class'], axis = 1)
+def split_data(data,k_features):
+    label = pd.DataFrame(data['class'])
+    data = data.drop(['class'], axis = 1)
+    data = select_features(data, label.values.ravel(), k_features)
+    data['class'] = label
+    mal_data = data[data['class'].isin([1])]
+    mal_data = mal_data.drop(['class'], axis = 1)
+    ben_data = data[data['class'].isin([0])]
+    ben_data = ben_data.drop(['class'],axis=1)
+    return mal_data,ben_data
 
-mal_api_data = app_api_data[app_api_data['class'].isin([1])]
-mal_api_data = mal_api_data.drop(['class'], axis = 1)
-ben_api_data = app_api_data[app_api_data['class'].isin([0])]
-ben_api_data = ben_api_data.drop(['class'], axis = 1)
-
-mal_method_data = app_method_data[app_method_data['class'].isin([1])]
-mal_method_data = mal_method_data.drop(['class'], axis = 1)
-ben_method_data = app_method_data[app_method_data['class'].isin([0])]
-ben_method_data = ben_method_data.drop(['class'], axis = 1)
-
-mal_data = pd.concat([mal_perm_data], axis = 1)#mal_perm_data, mal_api_data, mal_method_data
-ben_data = pd.concat([ben_perm_data], axis = 1)#ben_perm_data, ben_api_data, ben_method_data
-
-# axis = 1 : 每一行的值求和 
-#count_series = mal_method_data.sum(axis = 1)
-#drop_feature = [index for index in count_series.index if count_series[index] < 200]
-#print(len(drop_feature))
-mal_num = len(mal_data)
-ben_num = len(ben_data)
-test_size_ben_radio = 0.3
-test_size_mal_radio = ben_num * test_size_ben_radio / mal_num 
- 
-# 得到恶意软件和良性软件各自的训练集和测试集
-mal_data_train, mal_data_test ,mal_y_train,mal_y_test = train_test_split(mal_data, np.ones(len(mal_data), dtype=np.uint16), test_size=test_size_mal_radio ,random_state=2019,shuffle=True)
-ben_data_train, ben_data_test ,ben_y_train,ben_y_test = train_test_split(ben_data, np.zeros(len(ben_data), dtype=np.uint16), test_size=test_size_ben_radio ,random_state=2019,shuffle=True)
-
-X_test = pd.concat([mal_data_test, ben_data_test], axis = 0)
-Y_test = np.append(mal_y_test, ben_y_test)
-
-# --------------start下采样--------------------
-mal_select_radio = len(ben_data_train) / len(mal_data_train) 
-x1, mal_use_data, y1, y2 = train_test_split(mal_data_train, mal_y_train, test_size=mal_select_radio,random_state=2019,shuffle=True)
-# --------------end下采样--------------------
-X_train = pd.concat([mal_use_data, ben_data_train], axis = 0)
-Y_train = np.append(y2, ben_y_train)
-print(X_train.shape)
-y_2 = pd.Series(Y_train)
-print(y_2.value_counts())
-print(X_test.shape)
-y_1 = pd.Series(Y_test)
-print(y_1.value_counts())
-
-
-
+def under_sampling(mal_data, ben_data):
+    mal_num = len(mal_data)
+    ben_num = len(ben_data)
+    test_size_ben_radio = 0.3
+    test_size_mal_radio = ben_num * test_size_ben_radio / mal_num 
+     
+    # 得到恶意软件和良性软件各自的训练集和测试集
+    mal_data_train, mal_data_test ,mal_y_train,mal_y_test = train_test_split(mal_data, np.ones(len(mal_data), dtype=np.uint16), test_size=test_size_mal_radio ,random_state=2019,shuffle=True)
+    ben_data_train, ben_data_test ,ben_y_train,ben_y_test = train_test_split(ben_data, np.zeros(len(ben_data), dtype=np.uint16), test_size=test_size_ben_radio ,random_state=2019,shuffle=True)
+    
+    X_test = pd.concat([mal_data_test, ben_data_test], axis = 0)
+    Y_test = np.append(mal_y_test, ben_y_test)
+    
+    # --------------start下采样--------------------
+    mal_select_radio = len(ben_data_train) / len(mal_data_train) 
+    x1, mal_use_data, y1, y2 = train_test_split(mal_data_train, mal_y_train, test_size=mal_select_radio,random_state=2019,shuffle=True)
+    # --------------end下采样--------------------
+    X_train = pd.concat([mal_use_data, ben_data_train], axis = 0)
+    Y_train = np.append(y2, ben_y_train)
+    return X_train,X_test,Y_train,Y_test
 
 '''
 # 按列分开
@@ -107,7 +89,6 @@ app_perm_data = pd.concat([mal_perm_data, ben_perm_data], axis = 0)
 app_api_data = pd.concat([mal_api_data, ben_api_data], axis = 0)
 app_method_data = pd.concat([mal_method_data, ben_method_data], axis = 0)
 
-
 selector = SelectKBest(score_func=chi2, k=400)
 new_api_data = selector.fit_transform(app_api_data, label)
 selector1 = SelectKBest(score_func=chi2, k=800)
@@ -118,21 +99,6 @@ new_perm_data = selector2.fit_transform(app_perm_data, label)
 new_api_data = pd.DataFrame(new_api_data)
 new_perm_data = pd.DataFrame(new_perm_data)
 new_method_data = pd.DataFrame(new_method_data)
-
-
-# 修改列名
-col1 = []
-col2 = []
-for i in range(len(new_api_data.columns)):
-    col1.append('I' + str(i))
-for i in range(len(new_method_data.columns)):
-    col2.append('M' + str(i))
-    
-new_api_data.columns = col1
-new_method_data.columns = col2
-
-new_data =  pd.concat([new_api_data], axis = 1) #new_api_data,new_perm_data,new_method_data
-
 '''
 def print_res(Y_test, y_pre, classter):
     cnf_matrix = confusion_matrix(Y_test, y_pre)
@@ -198,10 +164,8 @@ def get_oof(clf,n_folds,X_train,y_train,X_test):
 def stacking(X_train, X_test, Y_train, Y_test):
     new_X_train = pd.DataFrame()
     new_X_test = pd.DataFrame()
-    select_classifier_num = 5
-    #['DT','KNN','NB','GDBC','LR','RF','SVM','MLP','XGB','LR']
-    #['DT','LR','RF','LR']
-    models = ['DT','KNN','NB','GBDT','LR','SVM','RF','MLP','XGB','LR']
+    #['DT','KNN','NB','GBDT','LR','SVM','RF','MLP','XGB']
+    models = ['DT','KNN','NB','GBDT','LR','SVM','RF','MLP','XGB']
 #    print("不集成的效果：")
 #    for model in models:
 #        train_predict(model,X_train, X_test, Y_train, Y_test)
@@ -211,26 +175,32 @@ def stacking(X_train, X_test, Y_train, Y_test):
         model1_train, model1_test = get_oof(clf, 5, X_train, Y_train, X_test)
         new_X_train[model_name] = model1_train
         new_X_test[model_name] = model1_test
-    print("集成后的效果:")
-    print("用所有算法：")
-    train_predict(models[-1],new_X_train,new_X_test,Y_train,Y_test)
-    
-    selector = SelectKBest(score_func=chi2, k=select_classifier_num)
-    new_X_train = selector.fit_transform(new_X_train, Y_train)
-    support = selector.get_support()
-    use_classifier = []
-    cloumns = new_X_test.columns
-    for i in range(len(cloumns)):
-        if support[i] == False:
-            new_X_test = new_X_test.drop([cloumns[i]], axis=1)
-        else :
-            use_classifier.append(cloumns[i])
-    print("挑选的算法：")
-    print(use_classifier)
-    # 第二次训练
-    train_predict(models[-1],new_X_train,new_X_test,Y_train,Y_test)
-    
+   
+    return new_X_train,new_X_test
+
+
     
 if __name__ == '__main__':
-    stacking(X_train, X_test, Y_train, Y_test)
-#    base_method(X_train, X_test, Y_train, Y_test)
+    mal_api_data, ben_api_data = split_data(app_api_data, 400)
+    mal_perm_data, ben_perm_data = split_data(app_perm_data,300)
+    mal_method_data, ben_method_data = split_data(app_method_data,800)
+    X_api_train, X_api_test, Y_api_train, Y_api_test = under_sampling(mal_api_data, ben_api_data)
+    X_perm_train, X_perm_test, Y_perm_train, Y_perm_test = under_sampling(mal_perm_data, ben_perm_data)
+    X_method_train, X_method_test, Y_method_train, Y_method_test = under_sampling(mal_method_data, ben_method_data)    
+
+
+#   stacking 第一次训练    
+    new_X_perm_train, new_X_perm_test = stacking(X_perm_train, X_perm_test, Y_perm_train, Y_perm_test)
+    new_X_api_train, new_X_api_test = stacking(X_api_train, X_api_test, Y_api_train, Y_api_test)
+    new_X_method_train, new_X_method_test = stacking(X_method_train, X_method_test, Y_method_train, Y_method_test)
+
+#    print("单独训练: permission、api、method")
+#    train_predict('XGB',new_X_perm_train,new_X_perm_test,Y_perm_train, Y_perm_test)
+#    train_predict('XGB',new_X_api_train,new_X_api_test,Y_perm_train, Y_perm_test)
+#    train_predict('XGB',new_X_method_train,new_X_method_test,Y_perm_train, Y_perm_test)
+    
+    new_X_train = (new_X_perm_train + new_X_api_train  + new_X_method_train) / 3
+    new_X_test = (new_X_perm_test + new_X_api_test  + new_X_method_test) / 3
+    # 第二次训练
+    print("融合训练：")
+    train_predict('XGB',new_X_train,new_X_test,Y_perm_train, Y_perm_test)
