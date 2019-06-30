66b205d80da34d6800326fcba033075adb70cca6
“xidianlian <727610307@qq.com>
2019-06-30 23:04:45 +0800

    add singal model cross_vald

--- Tracker.py
+++ Tracker.py
@@ -23,10 +23,7 @@ from sklearn.model_selection import cross_validate
 import xgboost as xgb
 from sklearn.metrics import accuracy_score,recall_score,precision_score,f1_score,classification_report,confusion_matrix
 
-'''
-Tracker 数据集
-'''
-def select_split_data(data,num,data_type):
+def chi_2(data,num,data_type):
     label = pd.DataFrame(data['class']).values.ravel()
     data = data.drop(['class'], axis = 1)
     selector = SelectKBest(score_func=chi2, k=num)
@@ -37,8 +34,7 @@ def select_split_data(data,num,data_type):
     for i in range(len(data.columns)):
         col.append(data_type+ str(i))
     data.columns = col
-    data_train, data_test ,y_train,y_test = train_test_split(data, label, test_size=0.3 ,random_state=2019,shuffle=True)
-    return data_train, data_test ,y_train,y_test
+    return data, label
 
 def print_res(Y_test, y_pre, classter):
     cnf_matrix = confusion_matrix(Y_test, y_pre)
@@ -46,11 +42,15 @@ def print_res(Y_test, y_pre, classter):
     print("FP : ", cnf_matrix[0,1])
     print("TN : ", cnf_matrix[0,0])
     print("FN : ", cnf_matrix[1,0])
-    print(classter + " accuracy" + ":", accuracy_score(Y_test,y_pre))
-    print(classter + " precision" + ":", precision_score(Y_test,y_pre))
-    print(classter + " recall" + ":", recall_score(Y_test, y_pre))
-    print(classter + " F1" + ":", f1_score(Y_test,y_pre))
-    print("***********************************")
+    acc = accuracy_score(Y_test,y_pre)
+    pre = precision_score(Y_test,y_pre)
+    rec = recall_score(Y_test, y_pre)
+    f1 = f1_score(Y_test,y_pre)
+    print("%s accuracy:%.4f   precision:%.4f   recall:%.4f   F1:%.4f" % (classter, acc,pre,rec,f1))   
+#    print(classter + " precision weight" + ":", precision_score(Y_test,y_pre,average='weighted'))
+#    print(classter + " recall wight" + ":", recall_score(Y_test, y_pre,average='weighted'))
+#    print(classter + " F1 wight" + ":", f1_score(Y_test,y_pre,average='weighted'))
+#    print("***********************************")
    
       
 def select_model(model_name):
@@ -83,7 +83,6 @@ def train_predict(modelname,X_train, X_test, Y_train, Y_test):
     y_pre = clf.predict(X_test)
     print_res(Y_test, y_pre, modelname)
     
-
 def get_oof(clf,n_folds,X_train,y_train,X_test):
     ntrain = X_train.shape[0]
     ntest =  X_test.shape[0]
@@ -107,11 +106,12 @@ def stacking(X_train, X_test, Y_train, Y_test):
     new_X_train = pd.DataFrame()
     new_X_test = pd.DataFrame()
     #['DT','KNN','NB','GBDT','LR','SVM','RF','MLP','XGB']
-    models = ['DT','GBDT','LR','RF','MLP']
+   
+    models = ['DT','KNN','GBDT','LR','SVM','RF','XGB']
 #    print("不集成的效果：")
 #    for model in models:
 #        train_predict(model,X_train, X_test, Y_train, Y_test)
-    
+#    
     for model_name in models[0 : len(models)]:
         clf = select_model(model_name)
         model1_train, model1_test = get_oof(clf, 5, X_train, Y_train, X_test)
@@ -121,53 +121,94 @@ def stacking(X_train, X_test, Y_train, Y_test):
     return new_X_train,new_X_test
 
 
-def start(app_api_data,app_perm_data,app_method_data):
-    X_api_train, X_api_test, Y_api_train, Y_api_test = select_split_data(app_api_data, 400,'api')
-    X_perm_train, X_perm_test, Y_perm_train, Y_perm_test = select_split_data(app_perm_data,'all','perm')
-    X_method_train, X_method_test, Y_method_train, Y_method_test = select_split_data(app_method_data,400,'method')
+def FeatDroid(app_api_data,app_perm_data,app_method_data,label):
     
-    X_train = pd.concat([X_api_train, X_perm_train, X_method_train], axis = 1) 
-    X_test = pd.concat([X_api_test, X_perm_test, X_method_test], axis = 1)
-    #   stacking 第一次训练    
-    new_X_train, new_X_test = stacking(X_train, X_test, Y_api_train, Y_api_test)
-    # 第二次训练
-    train_predict('LR',new_X_train,new_X_test,Y_api_train, Y_api_test)
-    
+    X_api_train, X_api_test, Y_api_train, Y_api_test = train_test_split(app_api_data, label, test_size=0.3 ,random_state=2019,shuffle=True)
+    X_perm_train, X_perm_test, Y_perm_train, Y_perm_test  = train_test_split(app_perm_data, label, test_size=0.3 ,random_state=2019,shuffle=True)
+    X_method_train, X_method_test, Y_method_train, Y_method_test = train_test_split(app_method_data, label, test_size=0.3 ,random_state=2019,shuffle=True)
     
     new_X_api_train, new_X_api_test = stacking(X_api_train, X_api_test, Y_api_train, Y_api_test)
     new_X_perm_train, new_X_perm_test = stacking(X_perm_train, X_perm_test, Y_perm_train, Y_perm_test)
     new_X_method_train, new_X_method_test = stacking(X_method_train, X_method_test, Y_method_train, Y_method_test)
-    print("融合训练1：")
-    new_X_train = (new_X_perm_train + new_X_api_train  + new_X_method_train) / 3
-    new_X_test = (new_X_perm_test + new_X_api_test  + new_X_method_test) / 3
-    train_predict('LR',new_X_train,new_X_test,Y_perm_train, Y_perm_test)
+#    print("融合训练1：")
+#    new_X_train = (new_X_perm_train + new_X_api_train  + new_X_method_train) / 3
+#    new_X_test = (new_X_perm_test + new_X_api_test  + new_X_method_test) / 3
+#    train_predict('LR',new_X_train,new_X_test,Y_perm_train, Y_perm_test)
     
-    print("融合训练2：")
-    new_X_train = pd.concat([new_X_perm_train, new_X_api_train, new_X_method_train], axis = 1)
-    new_X_test = pd.concat([new_X_perm_test, new_X_api_test, new_X_method_test], axis = 1)
-    train_predict('LR',new_X_train,new_X_test,Y_perm_train, Y_perm_test)
-    
+    alpha = np.arange(0.1, 1, 0.1)
+    for i in alpha:
+        for j in alpha:
+            for k in alpha:
+                if i + j + k == 1:
+                    print("i: %.2f, j: %.2f, k:%0.2f "%(i,j,k))
+                    new_X_train = i*new_X_perm_train + j*new_X_api_train  + k*new_X_method_train
+                    new_X_test = i*new_X_perm_test + j*new_X_api_test  + k*new_X_method_test
+                    print("融合训练：")
+                    train_predict('LR',new_X_train,new_X_test,Y_perm_train, Y_perm_test)
 
-def amd_data():
-    api_csv_path = "E:\\Spyder\\android_malware_detection\\input\\app_api_mal2000.csv"
-    perm_csv_path = "E:\\Spyder\\android_malware_detection\\input\\app_perm.csv"
-    method_csv_path = "E:\\Spyder\\android_malware_detection\\input\\app_method_mal5000.csv"
+def cross_validation(clf, data, label):
+    k = 5
+    acc = 0
+    pre = 0
+    rec = 0
+    f1 = 0
+    kf = KFold(n_splits=k, shuffle=True)
+    for train_index, vald_index in kf.split(data):
+        kf_X_train = data.iloc[train_index,:]
+        kf_X_vald = data.iloc[vald_index, :]
+        kf_y_train = label[train_index]
+        kf_y_test = label[vald_index]
+        clf = clf.fit(kf_X_train, kf_y_train)
+        kf_y_pre = clf.predict(kf_X_vald)
+        acc += accuracy_score(kf_y_test,kf_y_pre)
+        pre += precision_score(kf_y_test,kf_y_pre)
+        rec += recall_score(kf_y_test,kf_y_pre)
+        f1 += f1_score(kf_y_test,kf_y_pre)
+    return acc/k, pre/k, rec/k, f1/k
+
+def run_cross_vald(data, label):
+    models = ['DT','KNN','GBDT','LR','SVM','RF','XGB']
+    for model_name in models[0 : len(models)]:
+        clf = select_model(model_name)
+        acc,pre,rec,f1 = cross_validation(clf, data, label)
+        print("cross_validation %s, acc %.4f, pre %.4f, rec %.4f, F1 %.4f" %(model_name,acc,pre,rec,f1))
+     
+# 单个分类器做交叉验证
+def single_classfier_experiment(app_api_data,app_perm_data,app_method_data,label):
+    run_cross_vald(app_api_data, label)
+    run_cross_vald(app_perm_data, label)
+    run_cross_vald(app_method_data, label)
+
+def read_select_data(api_csv_path,perm_csv_path,method_csv_path):
     app_api_data = pd.read_csv(api_csv_path, dtype=np.uint16)
     app_perm_data = pd.read_csv(perm_csv_path, dtype=np.uint16)
     app_method_data = pd.read_csv(method_csv_path, dtype=np.uint16)
-    start(app_api_data,app_perm_data,app_method_data)
+    app_api_data, label = chi_2(app_api_data, 400,'api')
+    app_perm_data, label = chi_2(app_perm_data, 'all', 'perm')
+    app_method_data, label = chi_2(app_method_data, 800,'method')
+    return app_api_data,app_perm_data,app_method_data, label
+  
+def amd_data():
+    api_csv_path = "E:\\Spyder\\android_malware_detection\\input\\app_api_mal4000.csv"
+    perm_csv_path = "E:\\Spyder\\android_malware_detection\\input\\app_perm.csv"
+    method_csv_path = "E:\\Spyder\\android_malware_detection\\input\\app_method_mal5000.csv"
+    app_api_data,app_perm_data,app_method_data, label = read_select_data(api_csv_path,perm_csv_path,method_csv_path)
+    single_classfier_experiment(app_api_data,app_perm_data,app_method_data)
+    #FeatDroid(app_api_data,app_perm_data,app_method_data,label)
     
 def tracker_data():
     api_csv_path = "E:\\Spyder\\android_malware_detection\\input\\Tracker_Api.csv"
     perm_csv_path = "E:\\Spyder\\android_malware_detection\\input\\Tracker_Perm.csv"
     method_csv_path = "E:\\Spyder\\android_malware_detection\\input\\Tracker_Method.csv"
-    app_api_data = pd.read_csv(api_csv_path, dtype=np.uint16)
-    app_perm_data = pd.read_csv(perm_csv_path, dtype=np.uint16)
-    app_method_data = pd.read_csv(method_csv_path, dtype=np.uint16)
-    start(app_api_data,app_perm_data,app_method_data)
+    app_api_data,app_perm_data,app_method_data, label = read_select_data(api_csv_path,perm_csv_path,method_csv_path)
+    single_classfier_experiment(app_api_data,app_perm_data,app_method_data,label)
+    #FeatDroid(app_api_data,app_perm_data,app_method_data,label)
 
 if __name__ == '__main__':
-    amd_data()
+#    amd_data()
+#    print("-----------------------------------")
+#    print("-----------Tracker data------------")
+#    print("-----------------------------------")
     tracker_data()
     
     
